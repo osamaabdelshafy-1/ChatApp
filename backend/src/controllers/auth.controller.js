747facf8eb58e676cf74750f43f8e0bfb7ed8334
Osama-Abdelshafy <osamaabdelshafy7@gmail.com>
2025-09-23 14:40:35 +0300

    add  global middleware handler for error and routes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,103 +2,128 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/user.model.js");
 const generateToken = require("../utils/generateToken.js");
 const sendWelcomeEmail = require("../emails/emailHandler.js");
-const signUp = async (req, res) => {
+const asyncWrapper = require("../middlewares/asyncWrapper.js");
+const appError = require("../utils/appError.js");
+const httpsStatusText = require("../utils/httpsStatusText.js");
+
+const signUp = asyncWrapper(async (req, res, next) => {
   const { fullName, email, password } = req.body;
 
+  if (!fullName || !email || !password) {
+    const error = appError.create(
+      "All fields are required",
+      400,
+      httpsStatusText.FAIL
+    );
+
+    return next(error);
+  }
+  if (password.length < 6) {
+    return next(
+      appError.create(
+        "password must be at least 6 characters",
+        400,
+        httpsStatusText.FAIL
+      )
+    );
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!emailRegex.test(email)) {
+    return next(
+      appError.create("Invalid email format", 400, httpsStatusText.FAIL)
+    );
+  }
+  const user = await User.findOne({ email });
+  if (user) {
+    return next(
+      appError.create("email already exits !", 400, httpsStatusText.FAIL)
+    );
+  }
+
+  //convert the password to hashed password for encryption
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //create the user
+  const newUser = new User({ fullName, email, password: hashedPassword });
+
+  if (!newUser) {
+    return next(
+      appError.create("Invalid user data", 400, httpsStatusText.FAIL)
+    );
+  }
+
+  //save the user in the database
+  const savedUser = await newUser.save();
+
+  if (!savedUser) {
+    return next(
+      appError.create(
+        "User doesn't be saved in  DB!",
+        500,
+        httpsStatusText.ERROR
+      )
+    );
+  }
+
+  //generate a token to use it for accessing the routes
+  // payload , res object to save the token inside it
+  generateToken(savedUser._id, res);
+  res.status(201).json({
+    data: {
+      _id: savedUser._id,
+      fullName: savedUser.fullName,
+      email: savedUser.email,
+      profilePic: savedUser.profilePic,
+    },
+    message: "A new user is registered. ",
+  });
+
+  //sending welcome email
   try {
-    if (!fullName | !email | !password) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
-    if (password.length < 6) {
-      return res
-        .status(400)
-        .json({ message: "Password must be at least 6 characters" });
-    }
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!emailRegex.test(email)) {
-      return res.status(400).json({ message: "Invalid email format. " });
-    }
-    const user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({ message: "Email has already exited" });
-    }
-
-    //convert the password to hashed password for encryption
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    //create the user
-    const newUser = new User({ fullName, email, password: hashedPassword });
-    if (newUser) {
-      //save the user in the database
-      const savedUser = await newUser.save();
-
-      //generate a token to use it for accessing the routes
-      // payload , res object to save the token inside it
-      generateToken(savedUser._id, res);
-      res.status(201).json({
-        data: {
-          _id: savedUser._id,
-          fullName: savedUser.fullName,
-          email: savedUser.email,
-          profilePic: savedUser.profilePic,
-        },
-        message: "A new user is registered. ",
-      });
-      //sending welcome email
-      try {
-        await sendWelcomeEmail(
-          newUser.email,
-          newUser.fullName,
-          process.env.CLIENT_URL
-        );
-        console.log("calling the function");
-      } catch (e) {
-        console.error("failed to send welcome email,", e.message);
-      }
-    } else {
-      return res.status(400).json({
-        message: "Invalid user data.",
-      });
-    }
+    await sendWelcomeEmail(
+      newUser.email,
+      newUser.fullName,
+      process.env.CLIENT_URL
+    );
+    console.log("calling the function");
   } catch (e) {
-    console.log("Error in operation of the singUp ,", e.message);
-    res.status(500).json({ message: "internal server error" });
+    console.error("failed to send welcome email,", e.message);
   }
-};
+});
 
-const logIn = async (req, res) => {
+const logIn = asyncWrapper(async (req, res, next) => {
   const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: "Invalid credentials " });
 
-    const checkedPassword = await bcrypt.compare(password, user.password);
-    if (!checkedPassword)
-      return res.status(400).json({ message: "Invalid credentials " });
+  const user = await User.findOne({ email });
+  if (!user)
+    return next(
+      appError.create("Invalid credentials", 400, httpsStatusText.FAIL)
+    );
 
-    generateToken(user._id, res);
+  const checkedPassword = await bcrypt.compare(password, user.password);
+  if (!checkedPassword)
+    return next(
+      appError.create("Invalid credentials", 400, httpsStatusText.FAIL)
+    );
 
-    res.status(200).json({
-      data: {
-        _id: user._id,
-        fullName: user.fullName,
-        email: user.email,
-        profilePic: user.profilePic,
-      },
-      message: "user is registered",
-    });
-  } catch (error) {
-    console.error("error on the login controller,", error);
-    res.status(500).json({
-      message: "internal server error",
-    });
-  }
-};
-const logOut = async (_, res) => {
+  generateToken(user._id, res);
+  res.status(200).json({
+    data: {
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    },
+    message: "user is registered",
+  });
+});
+
+const logOut = asyncWrapper(async (_, res, next) => {
   res.cookie("jwt", "", { maxAge: 0 });
   res.status(200).json({ message: "logged out successfully" });
-};
+});
+
 module.exports = { signUp, logIn, logOut };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const authRoutes = require("./routes/auth.route");
 const connectDB = require("./lib/db");
-
+const httpsStatusText = require("./utils/httpsStatusText");
 require("dotenv").config();
 const app = express();
 
@@ -10,6 +10,23 @@ app.use("/api/v1/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
+app.use("/api" , (req, res) => {
+
+  return res.status(404).json({
+    status: httpsStatusText.ERROR,
+    data: null,
+    message: "Route not found",
+  });
+});
+app.use((error, req, res, next) => {
+
+  return res.status(error.statusCode || 500).json({
+    status: error.statusText || httpsStatusText.ERROR,
+    data: null,
+    message: error.message || "Internal Server Error",
+    code: error.statusCode || 500,
+  });
+});
 // connect to DB before server listening  to port for running the app
 connectDB()
   .then(() => {
